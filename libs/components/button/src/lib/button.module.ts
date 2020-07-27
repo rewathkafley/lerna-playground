6779f43d3fcdc61b360f85699176c5e763f3a93f
Rewath Kafley <rewath.kafley@selise.ch>
2020-07-27 12:18:49 +0600

    feat(buttom):button component

--- libs/components/button/src/lib/button.module.ts
+++ libs/components/button/src/lib/button.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonComponent } from './button.component';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule],
+  declarations: [ButtonComponent]
 })
-export class SfButtonModule {}
+export class SfButtonModule {
+}
